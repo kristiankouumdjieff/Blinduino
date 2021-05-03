9fb2c6f33e052de38c626fb07eef3617d5748caf
kristiankouumdjieff <kristian.kouumdjieff@gmail.com>
2021-05-03 02:18:13 +0100

    New empty accessory

---
+++ src/platformAccessory2.ts
@@ -0,0 +1,9 @@
+import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
+
+import { ExampleHomebridgePlatform } from './platform';
+
+/**
+ * Platform Accessory
+ * An instance of this class is created for each accessory your platform registers
+ * Each accessory may expose multiple services of different service types.
+ */
